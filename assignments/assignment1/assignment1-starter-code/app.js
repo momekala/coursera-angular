17584d4afac9d4233e751d4af2eb3f6d4306d7ad
patiboh <lapatiboh@gmail.com>
2016-09-21 16:29:33 +0200

    Commit first Angular Assignment

---
+++ assignments/assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,31 @@
+;(function () {
+'use strict'
+
+angular.module('CheckTooMuchApp', [])
+.controller('CheckTooMuchController', CheckTooMuchController)
+
+CheckTooMuchController.$inject = ['$scope']
+
+function CheckTooMuchController($scope) {
+  $scope.placeholder = "List comma separated dishes you usually have for lunch"
+  $scope.lunch = ""
+  $scope.success = "Enjoy!"
+  $scope.error = "Too much!"
+  $scope.warning = "Please enter data first"
+  $scope.spec = "Empty list items do NOT count as lunch items"
+
+  $scope.checkList = function () {
+    if ($scope.lunch.length == 0) {
+      $scope.message = $scope.warning
+    } else {
+      var lunchList = $scope.lunch.split(",")
+      var listUnique = lunchList.filter(function(value) {
+        return value.trim() !== ""
+      })
+      if(listUnique.length <= 3)
+        $scope.message = $scope.success
+      else $scope.message = $scope.error
+    }
+  }
+}
+})()
