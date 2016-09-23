4e7764ef0e92f21738dd8a8daacc33fd567f9825
patiboh <lapatiboh@gmail.com>
2016-09-23 18:16:22 +0200

    Assignment 2 - First run

--- assignments/assignment2/assignment2-starter-code/app.js
+++ assignments/assignment2/assignment2-starter-code/app.js
@@ -1,31 +1,80 @@
 ;(function () {
   'use strict'
 
+  var shoppingList = [
+    {
+      name: 'Milk',
+      quantity: '1l'
+    }, {
+      name: 'Orange Juice',
+      quantity: '1 bottle'
+    }, {
+      name: 'Cookie Dough',
+      quantity: 'A packet'
+    }, {
+      name: 'Chocolate',
+      quantity: '1 tablet'
+    }, {
+      name: 'Red Wine',
+      quantity: '1 bottle'
+    }, {
+      name: 'Apples',
+      quantity: '1k'
+    }, {
+      name: 'Bread',
+      quantity: '1 loaf'
+    }
+  ]
   angular.module('ShoppingListCheckOff', [])
-  .controller('CheckTooMuchController', CheckTooMuchController)
+    .controller('ToBuyShoppingController', ToBuyShoppingController)
+    .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
 
-  CheckTooMuchController.$inject = ['$scope']
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService']
+  function AlreadyBoughtShoppingController (ShoppingListCheckOffService) {
+    var itemsBoughtCtrl = this
 
-  function CheckTooMuchController ($scope) {
-    $scope.placeholder = 'List comma separated dishes you usually have for lunch'
-    $scope.lunch = ''
-    $scope.success = 'Enjoy!'
-    $scope.error = 'Too much!'
-    $scope.warning = 'Please enter data first'
-    $scope.spec = 'Empty list items do NOT count as lunch items'
+    itemsBoughtCtrl.getItems = function () {
+      return ShoppingListCheckOffService.getBoughtItems()
+    }
 
-    $scope.checkList = function () {
-      if ($scope.lunch.length === 0) {
-        $scope.message = $scope.warning
-      } else {
-        var lunchList = $scope.lunch.split(',')
-        var listUnique = lunchList.filter(function (value) {
-          return value.trim() !== ''
-        })
-        if (listUnique.length <= 3) {
-          $scope.message = $scope.success
-        } else $scope.message = $scope.error
-      }
+    itemsBoughtCtrl.isEmpty = function () {
+      return itemsBoughtCtrl.getItems().length === 0
+    }
+  }
+
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService']
+  function ToBuyShoppingController (ShoppingListCheckOffService) {
+    var itemsToBuyCtrl = this
+
+    itemsToBuyCtrl.getItems = function () {
+      return ShoppingListCheckOffService.getItemsToBuy()
+    }
+
+    itemsToBuyCtrl.isEmpty = function () {
+      return itemsToBuyCtrl.getItems().length === 0
+    }
+
+    itemsToBuyCtrl.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buyItem(itemIndex)
+    }
+  }
+
+  function ShoppingListCheckOffService () {
+    var service = this
+    var shoppingListToBuy = shoppingList
+    var alreadyBought = []
+
+    service.getItemsToBuy = function () {
+      return shoppingListToBuy
+    }
+    service.buyItem = function (itemIndex) {
+      var item = shoppingListToBuy[itemIndex]
+      shoppingListToBuy.splice(itemIndex, 1)
+      alreadyBought.push(item)
+    }
+    service.getBoughtItems = function () {
+      return alreadyBought
     }
   }
 })()
