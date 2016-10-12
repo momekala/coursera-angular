d34029f2ad30444eac41c9437ae69302219a4979
patiboh <lapatiboh@gmail.com>
2016-10-12 14:54:54 +0200

    Ca maaarcheeegsgs

--- assignments/assignment4-OK/src/menuApp/items.controller.js
+++ assignments/assignment4-OK/src/menuApp/items.controller.js
@@ -7,9 +7,8 @@
   ItemsController.$inject = ['MenuDataService', '$stateParams', 'list', 'items']
   function ItemsController (MenuDataService, $stateParams, list, items) {
     var categoryItems = this
-    categoryItems.items = items
-    var category = list.filter((item) => { return item.short_name == $stateParams.categoryShortName })
-    console.log(items)
+    categoryItems.items = items.menu_items
+    var category = list.filter((item) => { return item.short_name === $stateParams.categoryShortName })
     categoryItems.name = category[0].name
   }
 })()
